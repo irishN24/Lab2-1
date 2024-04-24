6740d3a53fe524642d4abf032f5e3edafe6ee41d
irishN24 <145177386+irishN24@users.noreply.github.com>
2024-04-24 15:36:18 +0700

    Add files via upload

---
+++ 2_1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int N;
+    int rez = scanf("%d", &N);
+    int V = 0;
+    int OST = 0;
+    int N2 = 1;
+    if (rez == 1)
+    {
+        if (N < 1){
+            N *= (-1);
+            N2 = 0;
+        }
+            while (N != 0)
+            {
+                OST = N % 10;
+                if (V < 0)
+                {
+                    V = 0;
+                    N = 0;
+                }
+                V = V * 10 + OST;
+                N = N / 10; 
+            }
+        if (N2 == 0){
+            V = V * (-1); 
+        }
+            if (V != 0) {
+                printf("%d", V);
+            }
+            else{
+                printf("0");
+            }
+
+    }
+    else
+    {
+        printf("WRONG DATA");
+    }
+
+
+    return 0;
+}
