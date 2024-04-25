fcd0b15e222faa75fcdc8917a68367bf6bc0f7a3
irishN24 <145177386+irishN24@users.noreply.github.com>
2024-04-25 09:24:46 +0700

    Update 2_1.c

--- 2_1.c
+++ 2_1.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
 int main()
-
 {
     int N;
     int rez = scanf("%d", &N);
     int V = 0;
     int OST = 0;
-    int N2 = 1;
+    int N2 = 0;
     if (rez == 1)
     {
-        if (N < 1){
+        if (N < 0){
             N *= (-1);
-            N2 = 0;
+            N2 = 1;
         }
-            while (N != 0)
+        while (N != 0)
+        {
+            OST = N % 10;
+            V = V * 10 + OST;
+            if (V < 0)
             {
-                OST = N % 10;
-                if (V < 0)
-                {
-                    V = 0;
-                    N = 0;
-                }
-                V = V * 10 + OST;
-                N = N / 10; 
+                V = 0;
+                N = 0;
             }
-        if (N2 == 0){
-            V = V * (-1); 
+
+            N = N / 10;
+        }
+        if (N2 == 1){
+            V = V * (-1);
         }
         printf("%d", V);
 
@@ -35,7 +35,5 @@ int main()
     {
         printf("WRONG DATA");
     }
-
-
     return 0;
 }
